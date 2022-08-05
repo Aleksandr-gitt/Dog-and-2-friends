ff51e9b44f4830352f95443197f7831eb98e80ae
Aleksandr-gitt <109696041+Aleksandr-gitt@users.noreply.github.com>
2022-08-05 22:05:20 +0300

    Update Program.cs

--- 3_tray/Program.cs
+++ 3_tray/Program.cs
@@ -1,22 +1,22 @@
 ﻿Console.WriteLine("Введите скорость первого друга ");
 //string input1 = Console.ReadLine(); 
-int FS1 = Int32.Parse(Console.ReadLine());  //Console.ReadLine() вместо input1
+int FS1 = Convert.ToInt32(Console.ReadLine());  //Console.ReadLine() вместо input1
 
 Console.WriteLine("Введите скорость второго друга ");
 //string input2 = Console.ReadLine(); 
-int FS2 = Int32.Parse(Console.ReadLine());  //Console.ReadLine() вместо input2
+int FS2 = Convert.ToInt32(Console.ReadLine());  //Console.ReadLine() вместо input2
 
 Console.WriteLine("Введите скорость собаки ");
 //string input3 = Console.ReadLine(); 
-int dogspeed = Int32.Parse(Console.ReadLine());   //Console.ReadLine() вместо input3
+int dogspeed = Convert.ToInt32(Console.ReadLine());   //Console.ReadLine() вместо input3
 
 Console.WriteLine("Введите расстояние между друзьями ");
 //string input4 = Console.ReadLine(); 
-int distance = Int32.Parse(Console.ReadLine());   //Console.ReadLine() вместо input4
+int distance = Convert.ToInt32(Console.ReadLine());   //Console.ReadLine() вместо input4
 
 Console.WriteLine("К кому начинает бежать собака ");
 //string input5 = Console.ReadLine(); 
-int friend = Int32.Parse(Console.ReadLine());   //Console.ReadLine() вместо input5
+int friend = Convert.ToInt32(Console.ReadLine());   //Console.ReadLine() вместо input5
 
 
 int count = 0;
